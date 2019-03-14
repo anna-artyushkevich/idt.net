48ca9960a061115fa26e700797daa647a0f4a3cb
Anna Artyushkevich <nurka.ann@gmail.com>
2019-03-14 22:47:31 +0300

    test new mobile anchor navigation

--- docs/js/back-to-top.js
+++ docs/js/back-to-top.js
@@ -7,6 +7,13 @@ $(document).ready(function(){
         }
     });
     $(window).scroll(function(){
+        if ($(this).scrollTop() > 30) {
+            $('#breadcrumbsNew').fadeIn("slow");
+        } else {
+            $('#breadcrumbsNew').fadeOut("slow");
+        }
+    });
+    $(window).scroll(function(){
         if ($(this).scrollTop() > 100) {
             $('#chatBot').fadeIn();
         } else {
@@ -14,6 +21,10 @@ $(document).ready(function(){
         }
     });
     $('#backToTop').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+    $('#backToTop1').click(function(){
         $("html, body").animate({ scrollTop: 0 }, 600);
         return false;
     });
@@ -159,38 +170,217 @@ $(document).ready(function(){
         $(this).parent().toggleClass('active');
     });
 
+    // Initial state
+    var scrollPos = 0;
+    var dataScrollDirection = 0;
+    // adding scroll event
+
+    function getScrollDirection(event){
+      // detects new state and compares it with the new one
+      if ((document.body.getBoundingClientRect()).top > scrollPos) {
+    		dataScrollDirection = 0;
+      } else {
+        dataScrollDirection = 1;
+      }
+    	// saves the new position for iteration.
+    	scrollPos = (document.body.getBoundingClientRect()).top;
+    }
+
     function onScroll(event){
-		var scrollPosition = $(document).scrollTop();
-        scrollPosition = scrollPosition + 90;
-		$('.active .link-parent-content .link').each(function () {
-			var currentLink = $(this);
-			var refElement = $(currentLink.attr("href"));
-			if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
-				$('.active .link-parent-content .link').removeClass("active-anchor");
-				currentLink.addClass("active-anchor");
-			}
-			else{
-				currentLink.removeClass("active-anchor");
-			}
-		});
-        $('.values .link').each(function () {
+    		var scrollPosition = $(document).scrollTop();
+            scrollPosition = scrollPosition + 90;
+    		$('.active .link-parent-content .link').each(function () {
+    			var currentLink = $(this);
+    			var refElement = $(currentLink.attr("href"));
+    			if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+    				$('.active .link-parent-content .link').removeClass("active-anchor");
+    				currentLink.addClass("active-anchor");
+    			}
+    			else{
+    				currentLink.removeClass("active-anchor");
+    			}
+    		});
+        $('#breadcrumbs .values .link').each(function () {
             var currentLink = $(this);
             var refElement = $(currentLink.attr("href"));
             var breadCrumb = $(this).attr("data-breadcrumb");
-            console.log("refElement: " + refElement);
-            console.log("breadCrumb: " + breadCrumb);
             if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
                 $('.value').attr("data-breadcrumb", breadCrumb);
-                var breadCrumb1 = $(this).attr("data-breadcrumb");
-                console.log("breadCrumb1: " + breadCrumb1);
             }
         });
-	}
+    }
+
+    function onScrollNew(event){
+        var scrollTopPosition = $(document).scrollTop();
+        var scrollBottomPosition = scrollTopPosition + window.innerHeight;
+        $('#breadcrumbsNew .values .link').each(function () {
+            var currentLink = $(this);
+            var refElement = $(currentLink.attr("href"));
+            var breadCrumb = $(this).attr("data-breadcrumb");
+            var up = $('.up');
+            var down = $('.down');
+            var title = $('.title');
+            var href = undefined;
+            var nextLink = undefined;
+            var backLink = undefined;
+            var data = undefined;
+            var nextRefElement = undefined;
+            var nextHref = undefined;
+            var nextBreadCrumb = undefined;
+
+            if (dataScrollDirection) {
+                if (refElement.position().top <= scrollBottomPosition && refElement.position().top + refElement.height() > scrollBottomPosition) {
+                    var top = refElement.position().top;
+                    var height = refElement.height();
+                    console.log("top: " + top + " / height: " + height + " / scrollBottomPosition: " + scrollBottomPosition);
+                    console.log("current refElement: " + refElement.attr('id') + " / current breadCrumb: " + breadCrumb);
+                    console.log("dataScrollDirection: " + dataScrollDirection);
+
+                    nextLink = $(this).next();
+                    console.log("---------------------");
+                    console.log("refElement: " + refElement.attr('id') + " / breadCrumb: " + breadCrumb);
+
+                    if (nextLink.length) {
+                        nextRefElement = $(nextLink.attr("href"));
+                        nextHref = nextLink.attr("href");
+                        nextBreadCrumb = $(nextLink).attr("data-breadcrumb");
+                        if (down.hasClass("vis")) {
+                            down.removeClass("vis");
+                        }
+                        if (!down.hasClass("w")) {
+                            down.addClass("w");
+                        }
+                        if (up.hasClass("w")) {
+                            up.removeClass("w");
+                        }
+                        if (title.hasClass("vis")) {
+                            title.removeClass("vis");
+                        }
+                        $('.title').attr("data-title", 'DOWN');
+                        console.log("nextRefElement: " + nextRefElement.attr('id') + " / nextBreadCrumb: " + nextBreadCrumb);
+                    } else {
+                        nextRefElement = 'The end';
+                        nextHref = '#';
+                        nextBreadCrumb = 'WHERE TO JUMP...';
+                        if (!down.hasClass("vis")) {
+                            down.addClass("vis");
+                        }
+                        if (!title.hasClass("vis")) {
+                            title.addClass("vis");
+                        }
+                        if (down.hasClass("w")) {
+                            down.removeClass("w");
+                        }
+                        console.log("nextRefElement: " + nextRefElement + " / nextBreadCrumb: " + nextBreadCrumb);
+                    }
+
+                    $('.value-new').attr("data-breadcrumb", nextBreadCrumb);
+                    backLink = $(this).prev();
+                    if (backLink.length) {
+                        href = backLink.attr("href");
+                        up.attr("href", href);
+                        if (up.hasClass("vis")) {
+                            up.removeClass("vis");
+                        }
+                    } else {
+                        if (!up.hasClass("vis")) {
+                            up.addClass("vis");
+                        }
+                    }
+                    down.attr("href", nextHref);
+                    console.log("up: " + up.attr("href") + " / down: " + down.attr("href"));
+                    nextLink = undefined;
+                    backLink = undefined;
+                    if (currentLink.is(":last-child")) {
+                      console.log("last");
+                    }
+                } else if (currentLink.is(":last-child") && refElement.position().top + refElement.height() < scrollBottomPosition && refElement.position().top + refElement.height() >= scrollTopPosition) {
+                    href = currentLink.attr("href");
+                    up.attr("href", href);
+                }
+            } else {
+                if ((refElement.position().top <= scrollTopPosition) && ((refElement.position().top + refElement.height()) > scrollBottomPosition)) {
+                    var top = refElement.position().top;
+                    var height = refElement.height();
+                    console.log("top: " + top + " / height: " + height + " / scrollTopPosition: " + scrollTopPosition + " / scrollBottomPosition: " + scrollBottomPosition);
+                    console.log("current refElement: " + refElement.attr('id') + " / current breadCrumb: " + breadCrumb);
+                    console.log("dataScrollDirection: " + dataScrollDirection);
+
+                    nextLink = $(this).prev();
+                    console.log("---------------------");
+                    console.log("refElement: " + refElement.attr('id') + " / breadCrumb: " + breadCrumb);
+
+                    if (nextLink.length) {
+                        nextRefElement = $(nextLink.attr("href"));
+                        nextHref = nextLink.attr("href");
+                        nextBreadCrumb = $(nextLink).attr("data-breadcrumb");
+                        if (up.hasClass("vis")) {
+                            up.removeClass("vis");
+                        }
+                        if (title.hasClass("vis")) {
+                            title.removeClass("vis");
+                        }
+                        if (!up.hasClass("w")) {
+                            up.addClass("w");
+                        }
+                        if (down.hasClass("w")) {
+                            down.removeClass("w");
+                        }
+                        $('.title').attr("data-title", 'UP');
+                        console.log("nextRefElement: " + nextRefElement.attr('id') + " / nextBreadCrumb: " + nextBreadCrumb);
+                    } else {
+                        nextRefElement = 'The end';
+                        nextHref = '#';
+                        nextBreadCrumb = 'WHERE TO JUMP...';
+                        if (!up.hasClass("vis")) {
+                            up.addClass("vis");
+                        }
+                        if (!title.hasClass("vis")) {
+                            title.addClass("vis");
+                        }
+                        if (up.hasClass("w")) {
+                            up.removeClass("w");
+                        }
+                        console.log("nextRefElement: " + nextRefElement + " / nextBreadCrumb: " + nextBreadCrumb);
+                    }
+
+                    $('.value-new').attr("data-breadcrumb", nextBreadCrumb);
+                    backLink = $(this).next();
+                    if (backLink.length) {
+                        href = backLink.attr("href");
+                        down.attr("href", href);
+                        if (down.hasClass("vis")) {
+                            down.removeClass("vis");
+                        }
+                    } else {
+                        if (!down.hasClass("vis")) {
+                            down.addClass("vis");
+                        }
+                    }
+                    up.attr("href", nextHref);
+                    console.log("up: " + up.attr("href") + " / down: " + down.attr("href"));
+                    nextLink = undefined;
+                    backLink = undefined;
+                } else if (currentLink.is(":last-child") && refElement.position().top + refElement.height() < scrollTopPosition) {
+                    nextBreadCrumb = currentLink.attr("data-breadcrumb");
+                    $('.value-new').attr("data-breadcrumb", nextBreadCrumb);
+                    if (title.hasClass("vis")) {
+                        title.removeClass("vis");
+                    }
+                    $('.title').attr("data-title", 'UP');
+                }
+            }
+        });
+    }
 
     stickyNav();
+    onScrollNew();
+    getScrollDirection();
     // and run it again every time you scroll
     $(window).scroll(function() {
         stickyNav();
+        getScrollDirection();
         onScroll();
+        onScrollNew();
     });
 });
